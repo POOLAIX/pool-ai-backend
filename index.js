2b3b881b8b1e5bdb04a57cf29f68069109a47ba4
POOLAIX <superdanielantonio@hotmail.com>
2025-07-06 21:03:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.post("/generate", async (req, res) => {
 
   try {
     const response = await openai.images.generate({
-      model: "dall-e-3",
+      model: "dall-e-2",
       prompt,
       n: 1,
       size: "1024x1024",
